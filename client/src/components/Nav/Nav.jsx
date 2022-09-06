900be68807653d92c27b8bcaeab64573f93ff6ac
LeoEchenique <leo.echeni@gmail.com>
2022-09-05 23:18:37 -0300

    Added styles for Home view

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,53 +1,50 @@
 import React from "react";
-import style from "../Nav/nav.module.css"
+import style from "../Nav/nav.module.css";
 import { Link } from "react-router-dom";
-import icon_dashboard from "../icons/icon_dashboard.png"
+import icon_dashboard from "../icons/icon_dashboard.png";
 import icon_operation from "../icons/icon_operationNav.png";
-import icon_user from "../icons/icon_user.png"
+import icon_user from "../icons/icon_user.png";
 import { useState, useEffect } from "react";
 
 import axios from "axios";
 
-
 export default function Nav() {
+  const [logged, setLogged] = useState({});
 
-    const [logged, setLogged]= useState({})
+  useEffect(() => {
+    let userLog = JSON.parse(localStorage.getItem("user"));
 
-    useEffect(() => {
-  
-    let userLog= JSON.parse(localStorage.getItem("user"))
+    setLogged(userLog);
+  }, []);
 
-    setLogged(userLog)
-    }, [])
+  const logout = () => {
+    localStorage.removeItem("user");
+    setLogged(null);
+    window.location.replace("http://localhost:3000/");
+  };
 
- 
-
-
-    const logout = () => {
-        localStorage.removeItem("user");
-        setLogged(null);
-        window.location.replace("http://localhost:3000/");
-      };
-
-  
-    
-    return (
-        <div className={style.nav_container}>
-            {logged ?
-                <div>
-                    <h1>Hi {logged.Name}!</h1>
-                    <img src={logged.Picture ? logged.Picture : icon_user} alt="" />
-                    <button onClick={() => logout()}>log out 🚀 </button>
-                  
-                </div>
-             
-                
-            : null}
-
-            <div className={style.nav_links}>
-                <Link className={style.a} to="/Home"><img src={icon_dashboard} alt=""/> Dashboard </Link>
-                <Link className={style.a} to="/Home/Operations/All"> <img src={icon_operation} alt="" /> All operations </Link>
-            </div>
+  return (
+    <div className={style.nav_container}>
+      {logged ? (
+        <div className={style.user_nav}>
+          <h1>Hi {logged.Name}!</h1>
+          <div>
+            <img src={logged.Picture ? logged.Picture : icon_user} alt="" />
+          </div>
         </div>
-    )
-}   
+      ) : null}
+      <div className={style.logOut}>
+              <button className={style.buttonLog} onClick={() => logout()}>log out 🚀 </button>
+      </div>
+      <div className={style.nav_links}>
+        <Link className={style.a} to="/Home">
+          <img src={icon_dashboard} alt="" /> Dashboard{" "}
+        </Link>
+        <Link className={style.a} to="/Home/Operations/All">
+          {" "}
+          <img src={icon_operation} alt="" /> All operations{" "}
+        </Link>
+      </div>
+    </div>
+  );
+}
